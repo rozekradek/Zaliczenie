7396cccfb65882d806d3dfd459fd6990bcbc6585
rozekradek <radek2003138@gmail.com>
2025-05-28 14:11:51 +0000

    zaliczenie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,34 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
-}
+        System.out.print("Podaj rozmiar tablicy: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Podaj " + n + " liczb całkowitych:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : array){
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        boolean foundduplicate = false;
+        System.out.println("Zduplikowane wartości");
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()){
+            if (entry.getValue() > 1){
+                System.out.println(entry.getKey());
+                foundduplicate = true;
+            }
+        }
+        if (!foundduplicate){
+            System.out.println("Brak zduplikowanych wartości");
+        }
+        scanner.close();
+        }
+    }
